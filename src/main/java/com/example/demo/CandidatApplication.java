c0ce5f593378a1b249f4f2dbcbe1fdfa590bd1d5
mustaphabenhadjyahia <mustapha.benhadjyahia@esprit.tn>
2020-10-13 15:34:15 +0100

    Eureka client

--- src/main/java/com/example/demo/CandidatApplication.java
+++ src/main/java/com/example/demo/CandidatApplication.java
@@ -5,9 +5,11 @@ import java.util.stream.Stream;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CandidatApplication {
 
 	public static void main(String[] args) {
